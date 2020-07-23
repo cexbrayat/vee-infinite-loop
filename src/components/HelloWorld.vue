aa986fe7f6de1baa39a1dc8ce20005acadd50df2
cexbrayat <cedric@ninja-squad.com>
2020-07-23 14:45:42 +0200

    feat: add vee-validate

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Register</h1>
+  <Form>
+    <Field as="input" rules="required" name="login" />
+    <ErrorMessage name="login" as="div"/>
+  </Form>
+
+</template>
+
+<script>
+import { defineRule, ErrorMessage, Form, Field } from 'vee-validate'
+import { required } from '@vee-validate/rules'
+
+defineRule('required', required);
+
+export default {
+  components: {
+    Form,
+    Field,
+    ErrorMessage
+  }
+}
+</script>
