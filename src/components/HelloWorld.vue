dff0a62dcfb7234a3a8b2883112ce4a6e9441e55
cexbrayat <cedric@ninja-squad.com>
2020-08-29 11:58:29 +0200

    repro: confirmed rule issue with beta.4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,50 @@
 <template>
-  <h1>Register</h1>
-  <Form>
-    <Field as="input" :rules="{ required: true }" name="login" />
-    <ErrorMessage name="login" as="div"/>
+  <Form @submit="register($event)" v-slot="{ meta: formMeta }">
+    <Field name="password" rules="required|min:3" v-slot="{ field, meta }" v-model="user.password">
+      <label for="password" :class="{ 'text-danger': meta.failed }">Password</label>
+      <input id="password" type="password" :class="{ 'is-invalid': meta.failed }" v-bind="field" />
+      <ErrorMessage name="password" class="error" />
+    </Field>
+    <Field
+      name="confirmPassword"
+      rules="required|confirmed:@password"
+      label="password confirmation"
+      v-slot="{ field, meta }"
+      v-model="user.confirmPassword"
+    >
+      <label for="confirm-password" :class="{ 'text-danger': meta.failed }">Confirm password</label>
+      <input id="confirm-password" type="password" :class="{ 'is-invalid': meta.failed }" v-bind="field" />
+      <ErrorMessage name="confirmPassword" class="error" />
+    </Field>
+    <button :disabled="formMeta.invalid">Register</button>
   </Form>
+  </template>
 
-</template>
+<script lang="ts">
+/* eslint-disable no-console */
+import { defineComponent, reactive } from 'vue';
+import { defineRule, ErrorMessage, Field, Form } from 'vee-validate';
+import { confirmed, min, required } from '@vee-validate/rules';
 
-<script>
-import { defineRule, ErrorMessage, Form, Field } from 'vee-validate'
-import { required } from '@vee-validate/rules'
-
+defineRule('min', min);
 defineRule('required', required);
+defineRule('confirmed', confirmed);
 
-export default {
+export default defineComponent({
+  name: 'Register',
   components: {
-    Form,
+    ErrorMessage,
     Field,
-    ErrorMessage
+    Form
+  },
+  setup() {
+    const user = reactive({ name: 'JB', password: '', confirmPassword: '' });
+
+    function register(values) {
+      console.log(values); // or console.log(user)
+    }
+
+    return { user, register };
   }
-}
+});
 </script>
