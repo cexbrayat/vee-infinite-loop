a1acaaa94cbcf512889fd8ee18afd4867d98d504
cexbrayat <cedric@ninja-squad.com>
2020-08-29 11:58:29 +0200

    repro: rc5 disabled

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,18 @@
 <template>
   <h1>Register</h1>
-  <Form>
-    <Field as="input" :rules="{ required: true }" name="login" />
-    <ErrorMessage name="login" as="div"/>
+  <Form @submit="register($event)" v-slot="{ meta: formMeta }">
+     <Field name="login" rules="required" v-slot="{ field }" v-model="user.name">
+      <label for="login">Login</label>
+      <input id="login" v-bind="field" />
+      <ErrorMessage name="login" />
+    </Field>
+    <button :disabled="formMeta.invalid">Register</button>
   </Form>
 
 </template>
 
 <script>
+import { reactive } from 'vue'
 import { defineRule, ErrorMessage, Form, Field } from 'vee-validate'
 import { required } from '@vee-validate/rules'
 
@@ -18,6 +23,13 @@ export default {
     Form,
     Field,
     ErrorMessage
+  },
+  setup() {
+    const user = reactive({ name: '' });
+      function register(values) {
+      console.log(values); // or console.log(user)
+    }
+    return { user, register };
   }
 }
 </script>
