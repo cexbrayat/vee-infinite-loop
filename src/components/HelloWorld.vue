a52f605f7250e6f14d60e1ead673cc5627625202
cexbrayat <cedric@ninja-squad.com>
2020-10-13 09:17:17 +0200

    repro: useField meta dirty/touched do not update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,41 @@
 <template>
-  <h1>Register</h1>
-  <Form>
-    <Field as="input" :rules="{ required: true }" name="login" />
-    <ErrorMessage name="login" as="div"/>
-  </Form>
-
+  <div class="row">
+    <div class="col-md-6 offset-md-3">
+      <h1>Sign up</h1>
+      <form @submit.prevent="register()">
+      {{ loginField.meta }}
+        <div class="form-group">
+          <label for="login">Login</label>
+          <input
+            id="login"
+            name="login"
+            class="form-control"
+            v-model="loginField.value"
+          />
+          <div v-if="loginField.meta.touched && !loginField.meta.valid" class="invalid-feedback d-block">The login is required.</div>
+        </div>
+        <button class="btn btn-primary" type="submit" :disabled="!formMeta.valid">Let's Go!</button>
+      </form>
+    </div>
+  </div>
 </template>
 
-<script>
-import { defineRule, ErrorMessage, Form, Field } from 'vee-validate'
+<script lang="ts">
+import { defineComponent, ref, reactive } from 'vue';
+import { useField, useForm, defineRule } from 'vee-validate';
 import { required } from '@vee-validate/rules'
 
 defineRule('required', required);
 
-export default {
-  components: {
-    Form,
-    Field,
-    ErrorMessage
+export default defineComponent({
+  name: 'Register',
+
+  setup() {
+    const { form, meta } = useForm();
+    const loginField = reactive(useField('login', { required: true }, { form }));
+    async function register() {
+    }
+    return { loginField, register, formMeta: reactive(meta) };
   }
-}
+});
 </script>
