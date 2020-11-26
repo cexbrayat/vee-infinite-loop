a1c0d4986b203928aac1a64b72a98439a51714ca
cexbrayat <cedric@ninja-squad.com>
2020-11-26 16:32:15 +0100

    repro: label is not watched

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,15 @@
 <template>
   <h1>Register</h1>
   <Form>
-    <Field as="input" rules="required" name="login" />
+    <Field as="input" rules="required" name="login" :label="label" />
     <ErrorMessage name="login" as="div"/>
+    <button @click="label = 'other'">Change label</button>
   </Form>
 
 </template>
 
 <script>
+import { ref } from 'vue';
 import { defineRule, ErrorMessage, Form, Field } from 'vee-validate'
 import { required } from '@vee-validate/rules'
 
@@ -18,6 +20,12 @@ export default {
     Form,
     Field,
     ErrorMessage
+  },
+  setup() {
+    const label = ref('login');
+    return {
+      label
+    }
   }
 }
 </script>
