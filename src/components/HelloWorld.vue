33a79695339ece5a2d50aeb768a7009d81cb24b6
cexbrayat <cedric@ninja-squad.com>
2020-12-09 14:07:11 +0100

    repro: cross field validation with context

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,39 @@
 <template>
   <h1>Register</h1>
-  <Form>
-    <Field as="input" rules="required" name="login" />
-    <ErrorMessage name="login" as="div"/>
+  <Form :initialValues="{ max: 5 }">
+    <div>
+      <label for="max">Max</label>
+      <Field name="max" v-slot="{ field }">
+        <input id="max" type="number" v-bind="field">
+      </Field>
+    </div>
+    <div>
+      <label for="number1">Number less than max field (using @max)</label>
+      <Field name="number1" rules="lessThan:@max" v-slot="{ field }">
+        <input id="number1" type="number" v-bind="field">
+      </Field>
+      <div>
+        <ErrorMessage name="number1" />
+      </div>
+    </div>
+    <div>
+      <label for="number2">Number less than max field (using context)</label>
+      <Field name="number2" rules="lessThanMax" v-slot="{ field }">
+        <input id="number2" type="number" v-bind="field">
+      </Field>
+      <div>
+        <ErrorMessage name="number2" />
+      </div>
+    </div>
   </Form>
 
 </template>
 
 <script>
 import { defineRule, ErrorMessage, Form, Field } from 'vee-validate'
-import { required } from '@vee-validate/rules'
 
-defineRule('required', required);
+defineRule('lessThan', (value, params) => Number(value) < Number(params[0]))
+defineRule('lessThanMax', (value, params, { form }) => Number(value) < Number(form.max))
 
 export default {
   components: {
